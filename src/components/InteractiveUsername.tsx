9a9b324cd9dc165a215c504f5ca220a89e408de2
horrorkist <horrorkist@gmail.com>
2023-11-29 00:31:55 +0900

    interactive username

---
+++ src/components/InteractiveUsername.tsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import FloatProfile from "./FloatProfile";
+import { useRef, useState } from "react";
+
+function InteractiveUsername({ username }: { username: string }) {
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+  const [show, setShow] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const onMouseOver = () => {
+    if (timer) clearTimeout(timer);
+
+    setTimer(
+      setTimeout(() => {
+        setShow(true);
+      }, 500)
+    );
+  };
+
+  const onMouseLeave = () => {
+    if (timer) clearTimeout(timer);
+
+    setShow(false);
+  };
+
+  return (
+    <span
+      onMouseOver={onMouseOver}
+      onMouseLeave={onMouseLeave}
+      className="font-medium text-black hover:text-gray-300 text-sm relative"
+    >
+      <Link to={`/${username}`}>{username}</Link>
+      {show && (
+        <div
+          ref={ref}
+          onClick={(e) => e.stopPropagation()}
+          className="absolute top-full w-[400px] z-50 overflow-hidden bg-white rounded-lg shadow-float flex justify-center items-center animate-fadeInWithoutScale"
+        >
+          <FloatProfile username={username} />
+        </div>
+      )}
+    </span>
+  );
+}
+
+export default InteractiveUsername;
